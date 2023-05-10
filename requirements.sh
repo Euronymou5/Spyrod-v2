302e49cf0387704c18992f34e4115f212e3dee39
Euronymou5 <85043356+Euronymou5@users.noreply.github.com>
2023-05-09 21:48:59 -0600

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,4 +1,23 @@
-apt install curl -y
-apt install python -y
-apt install python3 -y
-clear
+# Simple modificacion del archivo de instalacion requirements.sh
+
+function install() {
+   clear
+   echo -e "\033[32m[~] Actualizando paquetes..."
+   apt update
+
+   sleep 3
+   which python3 > /dev/null 2>&1
+   if [ "$?" -eq "0" ]; then
+   echo -e "\033[32m\n[~] Python3 ya esta instalado."
+   else
+   echo -e "\033[31m\n[!] Python3 no esta instalado."
+   sleep 2
+   echo -e "\033[32m\n[~] Instalando python3..."
+   apt install python3 -y
+   fi
+   
+   echo -e "\n\033[32m[✔] Instalacion completada."
+   echo -e "\n[~] Utiliza el comando: python3 spyrod.py para iniciar la herramienta."
+}
+
+install

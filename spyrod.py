4d4cd7f42280c7412ec4d5c32b1bf03a3da75887
JetBerri <84512017+JetBerri@users.noreply.github.com>
2024-01-30 19:47:47 +0100

    code optimization and upgrade spyrod.py
    Additional features have been added and the code has been reduced to half with the same features as the first version.

--- spyrod.py
+++ spyrod.py
@@ -1,239 +1,141 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-import requests, json
-import os
+# Updated version by JetnBerries https://github.com/JetBerri
+
+import requests
 import time
+import os
 
-class colores:
-    red="\033[31;1m"
+from colorama import Fore as color
 
+logo = (f"{color.RED}"
+        "             uu$:$:$:$:$:$uu\n"
+        "          uu$$$$$$$$$$$$$$$$$uu\n"
+        "         u$$$$$$$$$$$$$$$$$$$$$u\n"
+        "         u$$$$$$$$$$$$$$$$$$$$$$$u\n"
+        "       u$$$$$$$$$$$$$$$$$$$$$$$$$u\n"
+        "       u$$$$$$$$$$$$$$$$$$$$$$$$$u\n"
+        "       u$$$$$$*   *$$$*   *$$$$$$u\n"
+        "       *$$$$*      u$u       $$$$*\n"
+        "        $$$u       u$u       u$$$\n"
+        "        $$$u      u$$$u      u$$$\n"
+        "         *$$$$uu$$$   $$$uu$$$$*\n"
+        "          *$$$$$$$*   *$$$$$$$*\n"
+        "            u$$$$$$$u$$$$$$$u\n"
+        "             u$*$*$*$*$*$*$u\n"
+        "  uuu        $$u$ $ $ $ $u$$       uuu\n"
+        " u$$$$        $$u$u$u$u$u$$       u$$$$\n"
+        "  $$$$$uu      *$$$$$$$$$*     uu$$$$$$\n"
+        "u$$$$$$$$$$$      *****    uuuu$$$$$$$$$\n"
+        "$$$$***$$$$$$$$$$uuu   uu$$$$$$$$$***$$$\n"
+        "***      **$$$$$$$$$$$uu **$***\n"
+        "          uuuu **$$$$$$$$$$uuu\n"
+        " u$$$uuu$$$$$$$$$uu **$$$$$$$$$$$uuu$$$\n"
+        "$$$$$$$$$$****           **$$$$$$$$$$$*\n"
+        "   *$$$$$*                      **$$$$**\n"
+        "     $$$*    ___________________  $$$$*\n"
+        "            |Made by: Euronymou5|\n"
+        "            |___________________|\n"
+        "            | Spyrod Version: v4|\n"
+        "            |___________________|\n"
+        f"{color.RESET}")
 
 os.system("clear")
-logo = colores.red + '''
-             uu$:$:$:$:$:$uu
-          uu$$$$$$$$$$$$$$$$$uu
-         u$$$$$$$$$$$$$$$$$$$$$u
-         u$$$$$$$$$$$$$$$$$$$$$$$u
-       u$$$$$$$$$$$$$$$$$$$$$$$$$u
-       u$$$$$$$$$$$$$$$$$$$$$$$$$u
-       u$$$$$$*   *$$$*   *$$$$$$u
-       *$$$$*      u$u       $$$$*
-        $$$u       u$u       u$$$
-        $$$u      u$$$u      u$$$
-         *$$$$uu$$$   $$$uu$$$$*
-          *$$$$$$$*   *$$$$$$$*
-            u$$$$$$$u$$$$$$$u
-             u$*$*$*$*$*$*$u
-  uuu        $$u$ $ $ $ $u$$       uuu
- u$$$$        $$u$u$u$u$u$$       u$$$$
-  $$$$$uu      *$$$$$$$$$*     uu$$$$$$
-u$$$$$$$$$$$      *****    uuuu$$$$$$$$$
-$$$$***$$$$$$$$$$uuu   uu$$$$$$$$$***$$$*
- ***      **$$$$$$$$$$$uu **$***
-          uuuu **$$$$$$$$$$uuu
- u$$$uuu$$$$$$$$$uu **$$$$$$$$$$$uuu$$$
- $$$$$$$$$$****           **$$$$$$$$$$$*
-   *$$$$$*                      **$$$$**
-     $$$*    ___________________  $$$$*
-            |Made by: Euronymou5|
-            |___________________|
-            | Spyrod Version: v4|
-            |___________________|
-     
-'''
+print(logo)
 
-def per():
-  api2 = f"http://ip-api.com/json/?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,mobile,proxy,hosting,query&lang=es"
+def save_data_to_file(filename, data):
+    with open(filename, "w") as file:
+        for label, value in data.items():
+            file.write(f"[~] [{label}]: {value}\n")
 
-  time.sleep(1)
+def own_ip():
+    try:
+        respuesta = requests.get('https://httpbin.org/ip')
+        tu_ip = respuesta.json()['origin']
+        
+        return tu_ip
+    
+    except requests.RequestException as e:
+        print(f"Error al obtener la dirección IP: {e}")
 
-  data = requests.get(api2).json()
-  ##-----------Query---------##
-  print("\n[~] [IP]:", data['query'])
-  ##--------------ISP-----------
-  print("[~] [ISP] :", data['isp'])
-  if data['isp'] == False:
-    print('[~] [ISP no encontrado!]')
-  ##------------Org-----------##
-  print("[~] [Organizacion]:", data['org'])
-  if data['org'] == False:
-    print('[~] [Organizacion no encontrado!]')
-  ##-----------City---------##
-  print("[~] [Ciudad]:", data['city'])
-  if data['city'] == False:
-    print('[~] [Ciudad no encontrada!]')
-  ##-----------Country---------##
-  print("[~] [Nombre del país]:", data['country'])
-  if data['country'] == False:
-    print('[~] [Nombre del pais no encontrado!]')
-  ##----------Region-------##
-  print("[~] [Region]:", data['region'])
-  if data['region'] == False:
-    print('[~] [Region no encontrada!]')
-  ##---------Nombre del continente---
-  print("[~] [Nombre del continente]:", data['continent'])
-  if data['country'] == False:
-    print('[~] [Nombre del continente no encontrado!]')
-  #-----------Región / estado-------##
-  print("[~] [Región / estado]:", data['regionName'])
-  if data['regionName'] == False:
-    print('[~] [Region / Estado no encontrado!]')
-  ##----------2 letras continente##---
-  print("[~] [Código de continente de dos letras]:", data['continentCode'])
-  if data['country'] == False:
-    print('[~] [Código de continente de dos letras no encontrado!]')
-  #---Latitud----##
-  print("[~] [Latitud]:", data['lat'])
-  if data['lat'] == False:
-    print('[~] [Latitud no encontrada!]')
-  ##----------Longitud------##
-  print("[~] [Longitud]:", data['lon'])
-  if data['lon'] == False:
-    print('[~] [Longitud no encontrada!]')
-  ##--------------Timezone---------##
-  print("[~] [Zona horaria]:", data['timezone'])
-  if data['timezone'] == False:
-    print('[~] [Zona horaria no encontrada!]')
-  ##-------------- ZIP--------------##
-  print("[~] [Codigo zip]:", data['zip'])
-  if data['zip'] == False:
-    print('[~] [Codigo zip no encontrado!]')
-  ##------------ AS -------------------##
-  print("[~] [AS número y organización]:", data['as'])
-  if data['as'] == False:
-    print('[~] [AS número y organización no encontrado!]')
-  ##-----------Countrycode-----##
-  print("[~] [Código de país de dos letras]:", data['countryCode'])
-  if data['countryCode'] == False:
-    print('[~] [Código de país de dos letras no encontrado!]')
-  ##-----------Reverse IP---------##
-  print("[~] [DNS inverso de la IP]: ", data['reverse'])
-  if data['reverse'] == False:
-    print('[~] [DNS inverso de la IP!]')
-  ##--------------Mobile------##
-  print("[~] [Conexión móvil (celular)]:", data['mobile'])
-  if data['mobile'] == False:
-    print('[~] [Conexión móvil no encontrado!]')
-  #----currency----##
-  print('[~] [Moneda nacional]:', data['currency'])
-  if data['currency'] == False:
-    print('[~] [Moneda nacional no encontrada!]')
-  #-----district----#
-  print('[~] [Distrito (subdivisión de la ciudad)]:', data['district'])
-  if data['district'] == False:
-    print('[~] [Distrito no encontrado!]')
-  #-------Proxy-----#
-  print('[~] [Proxy, VPN o Tor]:', data['proxy'])
-  if data['proxy'] == False:
-    print('[~] [Proxy, VPN o Tor no encontrado!]')
+tu_ip = own_ip()    
 
-def locate():
-  print('\n[~] Escribe la IP de la victima')
-  ipin = input('[~] IP: ')
-  print(f'[~] Buscando datos de: {ipin}')
-  time.sleep(1)
-  api = f"http://ip-api.com/json/{ipin}?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,mobile,proxy,hosting,query&lang=es"
+def print_data(label, value):
+    if value:
+        print(f"[~] [{label}]: {value}")
+    else:
+        print(f"[~] [{label} no encontrado!]")
 
-  data = requests.get(api).json()
-  ##-----------Query---------##
-  print("\n[~] [IP De la victima]:", data['query'])
-  ##--------------ISP-----------
-  try:
-    print("[~] [ISP] :", data['isp'])
-    if data['isp'] == False:
-      print('[~] [ISP no encontrado!]')
-  ##------------Org-----------##
-    print("[~] [Organizacion]:", data['org'])
-    if data['org'] == False:
-     print('[~] [Organizacion no encontrado!]')
-  ##-----------City---------##
-    print("[~] [Ciudad]:", data['city'])
-    if data['city'] == False:
-     print('[~] [Ciudad no encontrada!]')
-  ##-----------Country---------##
-    print("[~] [Nombre del país]:", data['country'])
-    if data['country'] == False:
-     print('[~] [Nombre del pais no encontrado!]')
-  ##----------Region-------##
-    print("[~] [Region]:", data['region'])
-    if data['region'] == False:
-     print('[~] [Region no encontrada!]')
-  ##---------Nombre del continente---
-    print("[~] [Nombre del continente]:", data['continent'])
-    if data['country'] == False:
-     print('[~] [Nombre del continente no encontrado!]')
-  #-----------Región / estado-------##
-    print("[~] [Región / estado]:", data['regionName'])
-    if data['regionName'] == False:
-      print('[~] [Region / Estado no encontrado!]')
-  ##----------2 letras continente##---
-    print("[~] [Código de continente de dos letras]:", data['continentCode'])
-    if data['country'] == False:
-     print('[~] [Código de continente de dos letras no encontrado!]')
-  #---Latitud----##
-    print("[~] [Latitud]:", data['lat'])
-    if data['lat'] == False:
-     print('[~] [Latitud no encontrada!]')
-  ##----------Longitud------##
-    print("[~] [Longitud]:", data['lon'])
-    if data['lon'] == False:
-      print('[~] [Longitud no encontrada!]')
-  ##--------------Timezone---------##
-    print("[~] [Zona horaria]:", data['timezone'])
-    if data['timezone'] == False:
-      print('[~] [Zona horaria no encontrada!]')
-  ##-------------- ZIP--------------##
-    print("[~] [Codigo zip]:", data['zip'])
-    if data['zip'] == False:
-      print('[~] [Codigo zip no encontrado!]')
-  ##------------ AS -------------------##
-    print("[~] [AS número y organización]:", data['as'])
-    if data['as'] == False:
-      print('[~] [AS número y organización no encontrado!]')
-  ##-----------Countrycode-----##
-    print("[~] [Código de país de dos letras]:", data['countryCode'])
-    if data['countryCode'] == False:
-     print('[~] [Código de país de dos letras no encontrado!]')
-  ##-----------Reverse IP---------##
-    print("[~] [DNS inverso de la IP]: ", data['reverse'])
-    if data['reverse'] == False:
-      print('[~] [DNS inverso de la IP!]')
-  ##--------------Mobile------##
-    print("[~] [Conexión móvil (celular)]:", data['mobile'])
-    if data['mobile'] == False:
-      print('[~] [Conexión móvil no encontrado!]')
-  #----currency----##
-    print('[~] [Moneda nacional]:', data['currency'])
-    if data['currency'] == False:
-      print('[~] [Moneda nacional no encontrada!]')
-  #-----district----#
-    print('[~] [Distrito (subdivisión de la ciudad)]:', data['district'])
-    if data['district'] == False:
-      print('[~] [Distrito no encontrado!]')
-  #-------Proxy-----#
-    print('[~] [Proxy, VPN o Tor]:', data['proxy'])
-    if data['proxy'] == False:
-      print('[~] [Proxy, VPN o Tor no encontrado!]')
-  except KeyError:
-    print(f'\n[~] Error al intentar obtener datos de {ipin}')
+def display_location_data(data):
+    labels = ["IP De la victima", "ISP", "Organizacion", "Ciudad", "Nombre del país",
+              "Region", "Nombre del continente", "Región / estado", "Código de continente de dos letras",
+              "Latitud", "Longitud", "Zona horaria", "Codigo zip", "AS número y organización",
+              "Código de país de dos letras", "DNS inverso de la IP", "Conexión móvil (celular)",
+              "Moneda nacional", "Distrito (subdivisión de la ciudad)", "Proxy, VPN o Tor"]
+
+    for label in labels:
+        print_data(label, data.get(label.lower(), False))
+
+def locate(ipin):
+
+    api = f"http://ip-api.com/json/{ipin}?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,mobile,proxy,hosting,query&lang=es"
+
+    try:
+        data = requests.get(api).json()
+        print_data("IP De la victima", data.get('query', False))
+        print_data("ISP", data.get('isp', False))
+        print_data("Organizacion", data.get('org', False))
+        print_data("Ciudad", data.get('city', False))
+        print_data("Nombre del país", data.get('country', False))
+        print_data("Region", data.get('region', False))
+        print_data("Nombre del continente", data.get('continent', False))
+        print_data("Región / estado", data.get('regionName', False))
+        print_data("Código de continente de dos letras", data.get('continentCode', False))
+        print_data("Latitud", data.get('lat', False))
+        print_data("Longitud", data.get('lon', False))
+        print_data("Zona horaria", data.get('timezone', False))
+        print_data("Codigo zip", data.get('zip', False))
+        print_data("AS número y organización", data.get('as', False))
+        print_data("Código de país de dos letras", data.get('countryCode', False))
+        print_data("DNS inverso de la IP", data.get('reverse', False))
+        print_data("Conexión móvil (celular)", data.get('mobile', False))
+        print_data("Moneda nacional", data.get('currency', False))
+        print_data("Distrito (subdivisión de la ciudad)", data.get('district', False))
+        print_data("Proxy, VPN o Tor", data.get('proxy', False))
+        
+        save_data_to_file(f"{ipin}.txt", data)
+    
+    except Exception as e:
+        print(f'\n[~] Error al intentar obtener datos de {ipin}')
 
 def menu():
-  print(logo)
-  print('[1] Localizar una IP')
-  print('[2] Localizar tu propia IP')
-  print('[99] Salir')
-  var = int(input('\n>> '))
-  if var == 1:
-    locate()
-  elif var == 2:
-    per()
-  elif var == 99:
-    print('\n[~] Saliendo del programa...')
-    time.sleep(1)
-    exit()
-  else:
-    print('\n[~] Error opcion invalida.')
-    time.sleep(2)
-    menu()
+    
+    print('[1] Localizar una IP')
+    print('[99] Salir')
+    
+    try:
+        var = int(input('\n>> '))
+        if var == 1:
 
-menu()
+            time.sleep(1)
+            print("\n")
+            print(f"Tu direccion IP es : {tu_ip}")
+            print('\n[~] Escribe la IP de la victima')
+            ipin = input('[~] IP: ')
+            print(f'[~] Buscando datos de: {ipin}')
+            time.sleep(1)
+            locate(ipin)
+        elif var == 99:
+            print('\n[~] Saliendo del programa...')
+            time.sleep(1)
+            exit()
+        else:
+            print('\n[~] Error opcion invalida.')
+            time.sleep(2)
+            menu()
+    except ValueError:
+        print('\n[~] Por favor, ingresa un número válido.')
+        time.sleep(2)
+        menu()
+
+if __name__ == "__main__":
+
+    menu()
